0692a64bda9eeafbbfba41bc974a646085b03297
baknu <baknu@DESKTOP-D276CRT>
2022-08-04 15:41:15 +0900

    qqq123

---
+++ src/main/java/com/nadaena/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.nadaena.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+	@RequestMapping(value="/login", method = {RequestMethod.GET, RequestMethod.POST})
+	public String layer() {
+		System.out.println("login");
+		
+		return "user/loging";
+	}
+}
