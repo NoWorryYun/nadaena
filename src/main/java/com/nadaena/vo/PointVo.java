52441ce4131a52e1c74ab594724b8d64cc2c5ae6
midgjs <sjqksk.10@gmail.com>
2022-08-12 14:41:55 +0900

    point

---
+++ src/main/java/com/nadaena/vo/PointVo.java
@@ -0,0 +1,24 @@
+package com.nadaena.vo;
+
+public class PointVo {
+
+	private int pointNo;
+	private int userNo;
+	private int pointGroup;
+	private int charge;
+	private int amount;
+	private String pointDate;
+	private String source;
+	private String pointMemo;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
