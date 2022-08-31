345366520aebc90b058eab71412630eaddf7c946
y <y@DESKTOP-3945P2K>
2022-08-31 16:24:50 +0900

    커뮤니티2

--- src/main/java/com/nadaena/service/CommunityService.java
+++ src/main/java/com/nadaena/service/CommunityService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.nadaena.dao.CommunityDao;
 import com.nadaena.vo.BoardVo;
 
+@Service
 public class CommunityService {
 	
 	@Autowired
